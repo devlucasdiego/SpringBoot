af5376bc3ddf29edff5cafbfcfbadd96419e9aed
Lucas Diego <luucasdiego_12@hotmail.com>
2021-12-11 20:26:33 -0400

    Camada de Serviço

---
+++ src/main/java/br/com/fatecsenai/projetoSpringBoot/services/UserService.java
@@ -0,0 +1,25 @@
+package br.com.fatecsenai.projetoSpringBoot.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.fatecsenai.projetoSpringBoot.entities.User;
+import br.com.fatecsenai.projetoSpringBoot.repositories.UserRepository;
+
+@Service
+public class UserService {
+	@Autowired
+	private UserRepository repository;
+
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
