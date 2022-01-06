83a7b8f842bd3f663112b6d47d2da70bbd84926b
Lucas Diego <luucasdiego_12@hotmail.com>
2022-01-06 19:08:13 -0400

    Status do Pedido

--- src/main/java/br/com/fatecsenai/projetoSpringBoot/entities/enums/OrderStatus.java
+++ src/main/java/br/com/fatecsenai/projetoSpringBoot/entities/enums/OrderStatus.java
@@ -1,9 +1,24 @@
 package br.com.fatecsenai.projetoSpringBoot.entities.enums;
 
 public enum OrderStatus {
-	WAYTING_PAYMENT,
-	PAID,
-	SHIPPED,
-	DELIVERED,
-	CANCELED;
+	WAYTING_PAYMENT(1), PAID(2), SHIPPED(3), DELIVERED(4), CANCELED(5);
+
+	private int code;
+
+	private OrderStatus(int code) {
+		this.code = code;
+	}
+
+	public int getCode() {
+		return code;
+	}
+
+	public static OrderStatus valueOf(int code) {
+		for (OrderStatus value : OrderStatus.values()) {
+			if (value.getCode() == code) {
+				return value;
+			}
+		}
+		throw new IllegalArgumentException("OrderStatus CÓDIGO INVÁLIDO");
+	}
 }
