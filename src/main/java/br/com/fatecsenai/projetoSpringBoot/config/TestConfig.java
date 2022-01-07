27cd9962ef2fe75fe32f173f8f31bb1d29fd77fd
Lucas Diego <luucasdiego_12@hotmail.com>
2022-01-06 22:11:49 -0400

    Entidade Categoria

--- src/main/java/br/com/fatecsenai/projetoSpringBoot/config/TestConfig.java
+++ src/main/java/br/com/fatecsenai/projetoSpringBoot/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import br.com.fatecsenai.projetoSpringBoot.entities.Category;
 import br.com.fatecsenai.projetoSpringBoot.entities.Order;
 import br.com.fatecsenai.projetoSpringBoot.entities.User;
 import br.com.fatecsenai.projetoSpringBoot.entities.enums.OrderStatus;
+import br.com.fatecsenai.projetoSpringBoot.repositories.CategoryRepository;
 import br.com.fatecsenai.projetoSpringBoot.repositories.OrderRepository;
 import br.com.fatecsenai.projetoSpringBoot.repositories.UserRepository;
 
@@ -24,8 +26,18 @@ public class TestConfig implements CommandLineRunner {
 	@Autowired
 	private OrderRepository OrderRepository;
 
+	@Autowired
+	private CategoryRepository categoryRepository;
+
 	@Override
 	public void run(String... args) throws Exception {
+
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");
+
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
